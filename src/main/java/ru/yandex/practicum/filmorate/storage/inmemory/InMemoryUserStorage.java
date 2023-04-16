e426c84e6115d48bc444e9a908920b9a5a0bd9ae
fernandocortez8547 <108097219+fernandocortez8547@users.noreply.github.com>
2023-04-16 08:36:55 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/inmemory/InMemoryUserStorage.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 @Slf4j
