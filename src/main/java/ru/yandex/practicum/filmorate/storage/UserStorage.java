e7746b2bbc4af4a9229eb84758b60a2cee53f816
fernandocortez8547 <108097219+fernandocortez8547@users.noreply.github.com>
2023-04-16 08:35:47 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 public interface UserStorage {
     User addUser(User user);
